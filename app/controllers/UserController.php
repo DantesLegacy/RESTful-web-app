50498a9808d0a0badf4fe09bcfb2fda6dbf460bf
Joseph McNally <jmcna06@gmail.com>
2016-04-16 23:57:20 +0100

    [WAA-001] Initial commit of roughly working code.
    Still needs to be tested fully, but basic functionality
    seems to be working. Will use this as first alpha version.

---
+++ app/controllers/UserController.php
@@ -0,0 +1,141 @@
+<?php
+/*
+ * TODO: Create baseController class that extends this class in order to refactor code.
+ */
+class UserController {
+	private $slimApp;
+	private $model;
+	private $requestBody;
+	public function __construct($model, $action = null, $slimApp, $parameteres = null) {
+		$this->model = $model;
+		$this->slimApp = $slimApp;
+		$this->requestBody = json_decode ( $this->slimApp->request->getBody (), true ); // this must contain the representation of the new user
+		
+		if (! empty ( $parameteres [COLUMN_ID] ))
+			$id = $parameteres [COLUMN_ID];
+		
+		switch ($action) {
+			case ACTION_GET_USER :
+				$this->getUser ( $id );
+				break;
+			case ACTION_GET_USERS :
+				$this->getUsers ();
+				break;
+			case ACTION_UPDATE_USER :
+				$this->updateUser ( $id, $this->requestBody );
+				break;
+			case ACTION_CREATE_USER :
+				$this->createNewUser ( $this->requestBody );
+				break;
+			case ACTION_DELETE_USER :
+				$this->deleteUser ( $id );
+				break;
+			case ACTION_SEARCH_USERS :
+				$string = $parameteres ["SearchingString"];
+				$this->searchUsers ( $string );
+				break;
+			case null :
+				$this->slimApp->response ()->setStatus ( HTTPSTATUS_BADREQUEST );
+				$Message = array (
+						GENERAL_MESSAGE_LABEL => GENERAL_CLIENT_ERROR 
+				);
+				$this->model->apiResponse = $Message;
+				break;
+		}
+	}
+	private function getUsers() {
+		$answer = $this->model->getUsers ();
+		if ($answer != null) {
+			$this->slimApp->response ()->setStatus ( HTTPSTATUS_OK );
+			$this->model->apiResponse = $answer;
+		} else {
+			$this->slimApp->response ()->setStatus ( HTTPSTATUS_NOCONTENT );
+			$Message = array (
+					GENERAL_MESSAGE_LABEL => GENERAL_NOCONTENT_MESSAGE 
+			);
+			$this->model->apiResponse = $Message;
+		}
+	}
+	
+	private function getUser($userID) {
+		$answer = $this->model->getUser ( $userID );
+		if ($answer != null) {
+			$this->slimApp->response ()->setStatus ( HTTPSTATUS_OK );
+			$this->model->apiResponse = $answer;
+		} else {
+			
+			$this->slimApp->response ()->setStatus ( HTTPSTATUS_NOCONTENT );
+			$Message = array (
+					GENERAL_MESSAGE_LABEL => GENERAL_NOCONTENT_MESSAGE 
+			);
+			$this->model->apiResponse = $Message;
+		}
+	}
+	
+	private function createNewUser($newUser) {
+		if ($newID = $this->model->createNewUser ( $newUser )) {
+			$this->slimApp->response ()->setStatus ( HTTPSTATUS_CREATED );
+			$Message = array (
+					GENERAL_MESSAGE_LABEL => GENERAL_RESOURCE_CREATED,
+					COLUMN_ID => "$newID" 
+			);
+			$this->model->apiResponse = $Message;
+		} else {
+			$this->slimApp->response ()->setStatus ( HTTPSTATUS_BADREQUEST );
+			$Message = array (
+					GENERAL_MESSAGE_LABEL => GENERAL_INVALIDBODY 
+			);
+			$this->model->apiResponse = $Message;
+		}
+	}
+	private function deleteUser($userId) {
+		$answer = $this->model->deleteUser($userID);
+		if ($answer != NULL) {
+			/* Valid status codes for this are 200 or 204 */
+			$this->slimApp->response()->setStatus(HTTPSTATUS_OK);
+			$Message = array(
+				GENERAL_SUCCESS_MESSAGE => GENERAL_RESOURCE_DELETED
+			);
+			$this->model->apiResponse = $Message;
+		} else {
+			$this->slimAppresponse()->setStatus(HTTPSTATUS_BADREQUEST);
+			$Message = array(
+				GENERAL_ERROR_MESSAGE => GENERAL_DELETE_ERROR
+			);
+			$this->model->apiResponse = $Message;
+		}
+	}
+	
+	private function updateUser($userId, $userNewRepresentation) {
+		$answer = $this->model->updateUsers($userId, $userNewRepresentation);
+		if ($answer != NULL) {
+			/* Valid status codes for this are 200 or 204 */
+			$this->slimApp->response()->setStatus(HTTPSTATUS_OK);
+			$Message = array(
+				GENERAL_SUCCESS_MESSAGE => GENERAL_RESOURCE_UPDATED
+			);
+			$this->model->apiResponse = $Message;
+		} else {
+			$this->slimApp->response()->setStatus(HTTPSTATUS_BADREQUEST);
+			$Message = array(
+				GENERAL_ERROR_MESSAGE => GENERAL_UPDATE_ERROR
+			);
+			$this->model->apiResponse = $Message;
+		}
+	}
+	
+	private function searchUsers($string) {
+		$answer = $this->model->searchUsers($string);
+		if ($answer != null) {
+			$this->slimApp->response()->setStatus(HTTPSTATUS_OK);
+			$this->model->apiResponse = $answer;
+		} else {
+			$this->slimApp->response()->setStatus(HTTPSTATUS_NO_CONTENT);
+			$Message = array(
+				GENERAL_ERROR_MESSAGE => GENERAL_SEARCH_ERROR
+			);
+			$this->model->apiResponse = $Message;
+		}
+	}
+}
+?>
---
+++ app/models/UserModel.php
@@ -0,0 +1,90 @@
+<?php
+require_once "DB/pdoDbManager.php";
+require_once "DB/DAO/UsersDAO.php";
+require_once "Validation.php";
+class UserModel {
+	private $UsersDAO; // list of DAOs used by this model
+	private $dbmanager; // dbmanager
+	public $apiResponse; // api response
+	private $validationSuite; // contains functions for validating inputs
+	public function __construct() {
+		$this->dbmanager = new pdoDbManager ();
+		$this->UsersDAO = new UsersDAO ( $this->dbmanager );
+		$this->dbmanager->openConnection ();
+		$this->validationSuite = new Validation ();
+	}
+	public function getUsers() {
+		return ($this->UsersDAO->get ());
+	}
+	public function getUser($userID) {
+		if (is_numeric ( $userID ))
+			return ($this->UsersDAO->get ( $userID ));
+		
+		return false;
+	}
+	/**
+	 *
+	 * @param array $UserRepresentation:
+	 *        	an associative array containing the detail of the new user
+	 */
+	public function createNewUser($newUser) {
+		// validation of the values of the new user
+		
+		// compulsory values
+		if (! empty ( $newUser [COLUMN_NAME] ) && ! empty ( $newUser [COLUMN_SURNAME] )
+			&& ! empty ( $newUser [COLUMN_EMAIL] ) && ! empty ( $newUser [COLUMN_PASSWORD] )) {
+			/*
+			 * the model knows the representation of a user in the database and this is:
+			 * name: varchar(25) surname: varchar(25) email: varchar(50) password: varchar(40)
+			 */
+			
+			if (($this->validationSuite->isLengthStringValid ( $newUser [COLUMN_NAME], TABLE_USER_NAME_LENGTH ))
+				&& ($this->validationSuite->isLengthStringValid ( $newUser [COLUMN_SURNAME], TABLE_USER_SURNAME_LENGTH ))
+				&& ($this->validationSuite->isLengthStringValid ( $newUser [COLUMN_EMAIL], TABLE_USER_EMAIL_LENGTH ))
+				&& ($this->validationSuite->isLengthStringValid ( $newUser [COLUMN_PASSWORD], TABLE_USER_PASSWORD_LENGTH ))) {
+				if ($newId = $this->UsersDAO->insert ( $newUser ))
+					return ($newId);
+			}
+		}
+		
+		// if validation fails or insertion fails
+		return (false);
+	}
+	public function updateUsers($userID, $userNewRepresentation) {
+		/* Validate the fields being entered for update of user */
+		/* If field is not empty and does not fit DB limits */
+		if ((!empty($userNewRepresentation[COLUMN_NAME])
+				&& (!$this->validationSuite->isLengthStringValid($userNewRepresentation[COLUMN_NAME], TABLE_USER_NAME_LENGTH)))
+			|| (!empty($userNewRepresentation[COLUMN_SURNAME])
+				&& (!$this->validationSuite->isLengthStringValid($userNewRepresentation[COLUMN_SURNAME], TABLE_USER_SURNAME_LENGTH)))
+			|| (!empty($userNewRepresentation[COLUMN_EMAIL])
+				&& (!$this->validationSuite->isLengthStringValid($userNewRepresentation[COLUMN_EMAIL], TABLE_USER_EMAIL_LENGTH))
+				/* NOTE: Checking the validity of the email address in the Model Layer
+				 * as we don't want invalid data entering the database. It does not fit
+				 * in the Controller Layer as the format of the data is not it's concern */
+				&& (!$this->validationSuite->isEmailValid($userNewRepresentation[COLUMN_EMAIL])))
+			|| (!empty($userNewRepresentation[COLUMN_PASSWORD])
+				&& (!$this->validationSuite->isLengthStringValid($userNewRepresentation[COLUMN_PASSWORD], TABLE_USER_PASSWORD_LENGTH))))
+			return false;
+			
+		return ($this->UsersDAO->update($userID, $userNewRepresentation));
+	}
+	public function searchUsers($searchUserStr) {
+		/* If field is not empty and does not fit DB limits */
+		if ((!empty($searchUserStr[COLUMN_NAME])
+				&& (!$this->validationSuite->isLengthStringValid($userNewRepresentation[COLUMN_NAME], TABLE_USER_NAME_LENGTH)))
+			|| (!empty($userNewRepresentation[COLUMN_SURNAME])
+				&& (!$this->validationSuite->isLengthStringValid($userNewRepresentation[COLUMN_SURNAME], TABLE_USER_SURNAME_LENGTH))))
+			return false;	
+			
+		return ($this->UsersDAO->search($searchUserStr));
+	}
+	public function deleteUser($userID) {
+		return ($this->UsersDAO->delete($userID));
+	}
+	public function __destruct() {
+		$this->UsersDAO = null;
+		$this->dbmanager->closeConnection ();
+	}
+}
+?>
